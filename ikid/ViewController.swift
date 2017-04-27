7655aa7b626d85545cebaf4c69db163a9575b427
Vicky Juan <vicky37@uw.edu>
2017-04-26 19:46:52 -0700

    Initial Commit

---
+++ ikid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Vicky Juan on 4/26/17.
+//  Copyright © 2017 Victoria Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
