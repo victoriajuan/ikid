04d6dbad9e840ee3118ec2e35e768dc97f7e5b6b
Vicky Juan <vicky37@uw.edu>
2017-04-27 12:30:56 -0700

    functionality error with goodViewController, but pun and dad works

---
+++ ikid/DadViewController.swift
@@ -0,0 +1,86 @@
+//
+//  DadViewController.swift
+//  ikid
+//
+//  Created by Vicky Juan on 4/27/17.
+//  Copyright © 2017 Victoria Juan. All rights reserved.
+//
+
+import UIKit
+
+class DadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        firstViewController = nil;
+        secondViewController = nil;
+    }
+    var firstViewController : DadFirstViewController!
+    var secondViewController : DadSecondViewController!
+    
+    fileprivate func firstBuilder() {
+        if firstViewController == nil {
+            firstViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Fifth")
+                as! DadFirstViewController
+        }
+    }
+    fileprivate func secondBuilder() {
+        if secondViewController == nil {
+            secondViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Sixth")
+                as! DadSecondViewController
+        }
+    }
+    
+    @IBAction func switchViews(_ sender: Any) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        } else {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
+
+
+}
---
+++ ikid/GoodViewController.swift
@@ -0,0 +1,86 @@
+//
+//  GoodViewController.swift
+//  ikid
+//
+//  Created by Vicky Juan on 4/27/17.
+//  Copyright © 2017 Victoria Juan. All rights reserved.
+//
+
+import UIKit
+
+class GoodViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        firstViewController = nil;
+        secondViewController = nil;
+    }
+    var firstViewController : GoodFirstViewController!
+    var secondViewController : GoodSecondViewController!
+    
+    fileprivate func firstBuilder() {
+        if firstViewController == nil {
+            firstViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "First")
+                as! GoodFirstViewController
+        }
+    }
+    fileprivate func secondBuilder() {
+        if secondViewController == nil {
+            secondViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Second")
+                as! GoodSecondViewController
+        }
+    }
+    
+    @IBAction func switchViews(_ sender: Any) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        } else {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
+
+}
